4d5c3d7947a2becf12c0a0a9717ecd9902e455e6
Galahane Rouillé-Poirel <galahane.rouille@gmail.com>
2025-01-02 12:34:04 -0300

    Setup base localization system

---
+++ public/flags.js
@@ -0,0 +1,8 @@
+document.write('\
+\
+    <div class="languages">\
+        <img src="/images/Flags/flag_uk.gif" onclick="changeLanguage(\'en\', true)">\
+        <img src="/images/Flags/flag_fr.gif" onclick="changeLanguage(\'fr\', true)">\
+    </div>\
+\
+');
---
+++ public/language.js
@@ -0,0 +1,95 @@
+const savedVar = "selectedLanguageBellaSaraNeoCity";
+var currentLang = 'en';
+
+var localizedElements;
+var localizedDatas;
+var defaultLocas;
+
+// Used for first time users
+function checkBrowserLanguage() {
+    var lang = navigator.language;
+
+    // Add the new languages to the fail-safe to make sure all variants will be shown correctly
+    if (lang.includes('fr'))
+        lang = 'fr';
+    else        
+        lang = 'en';
+
+    changeLanguage(lang);
+}
+
+function saveLanguagePref(langSelected) {
+    localStorage.setItem(savedVar, langSelected);
+}
+
+function loadLanguagePref(updatePage = true) {
+
+    // Get the elements to update with the json
+    localizedElements = document.querySelectorAll('[data-il8n]');
+
+    // Load the language to use
+    if (localStorage.getItem(savedVar)) {
+        var langSaved = localStorage.getItem(savedVar);
+
+        currentLang = langSaved;
+
+        if (updatePage)
+            changeLanguage(langSaved);
+    }
+    else if (updatePage){
+        checkBrowserLanguage();
+    }
+}
+
+async function changeLanguage(langSelected, reload = false) {
+    var lang = langSelected;
+
+    currentLang = lang;
+
+    saveLanguagePref(lang);
+    //console.log(lang);
+
+    if (reload)
+        location.reload();
+    else
+    {
+        // Load the json file to use
+        var pathjson = '/localization/neocitiesLoca_' + lang + '.json';
+        var pathjsonDefault = '/localization/neocitiesLoca_en.json';
+        const jsonFile = await fetch(pathjson)
+                            .then(function(file) {return file.json();})
+                            .then(function(json) {
+                                localizedDatas = json;
+                            })
+
+        const jsonFileDefault = await fetch(pathjsonDefault)
+        .then(function(file) {return file.json();})
+        .then(function(json) {
+            defaultLocas = json;
+        })
+
+        // Update the texts elements based on HTML
+        localizedElements.forEach(element => 
+            {
+                var locaKey = element.getAttribute('data-il8n');
+                element.innerHTML = getLocalizedText(locaKey);
+            }
+        )    
+
+        document.documentElement.lang = lang;
+    }
+}
+
+function getCurrentLanguageCode()
+{
+    return currentLang;
+}
+
+function getLocalizedText(key)
+{
+    var txt = localizedDatas[key];
+    if (txt == undefined) // Go back to english if the translation is not done yet
+         return defaultLocas[key];
+
+    return txt;
+}
