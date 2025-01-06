2358f90cce20a4bcd4617ae256603781172227e1
SinGala <galahane.rouille@gmail.com>
2025-01-07 00:26:36 +0100

    Uncomment flags

--- public/flags.js
+++ public/flags.js
@@ -2,28 +2,28 @@ document.write('\
 \
     <div class="languages">\
         <img src="/images/Flags/flag_us.gif" onclick="changeLanguage(\'en\', true)" title="United States" alt="United States">\
-        <!--<img src="/images/Flags/flag_it.gif" onclick="changeLanguage(\'it\', true)" title="Italia" alt="Italia">-->\
-        <!--<img src="/images/Flags/flag_fr.gif" onclick="changeLanguage(\'fr\', true)" title="France" alt="France">-->\
-        <!--<img src="/images/Flags/flag_de.gif" onclick="changeLanguage(\'de\', true)" title="Deutschland" alt="Deutschland">-->\
-        <!--<img src="/images/Flags/flag_dk.gif" onclick="changeLanguage(\'da\', true)" title="Danmark" alt="Danmark">-->\
-        <!--<img src="/images/Flags/flag_se.gif" onclick="changeLanguage(\'sv\', true)" title="Sverige" alt="Sverige">-->\
-        <!--<img src="/images/Flags/flag_no.gif" onclick="changeLanguage(\'no\', true)" title="Norge" alt="Norge">-->\
-        <!--<img src="/images/Flags/flag_fi.gif" onclick="changeLanguage(\'fi\', true)" title="Suomi" alt="Suomi">-->\
-        <!--<img src="/images/Flags/flag_ru.gif" onclick="changeLanguage(\'ru\', true)" title="Россия" alt="Россия">-->\
-        <!--<img src="/images/Flags/flag_cl.gif" onclick="changeLanguage(\'es\', true)" title="Chile" alt="Chile">-->\
-        <!--<img src="/images/Flags/flag_mx.gif" onclick="changeLanguage(\'es\', true)" title="México" alt="México">-->\
-        <!--<img src="/images/Flags/flag_es.gif" onclick="changeLanguage(\'es\', true)" title="España" alt="España">-->\
-        <!--<img src="/images/Flags/flag_hr.gif" onclick="changeLanguage(\'hr\', true)" title="Hrvatska" alt="Hrvatska">-->\
-        <!--<img src="/images/Flags/flag_si.gif" onclick="changeLanguage(\'sl\', true)" title="Slovenija" alt="Slovenija">-->\
-        <!--<img src="/images/Flags/flag_uk.gif" onclick="changeLanguage(\'en\', true)" title="United Kingdom" alt="United Kingdom">-->\
-        <!--<img src="/images/Flags/flag_ca.gif" onclick="changeLanguage(\'en\', true)" title="Canada" alt="Canada">-->\
-        <!--<img src="/images/Flags/flag_au.gif" onclick="changeLanguage(\'en\', true)" title="Australia" alt="Australia">-->\
-        <!--<img src="/images/Flags/flag_za.gif" onclick="changeLanguage(\'af\', true)" title="South Africa" alt="South Africa">-->\
-        <!--<img src="/images/Flags/flag_nl.gif" onclick="changeLanguage(\'nl\', true)" title="Nederland" alt="Nederland">-->\
-        <!--<img src="/images/Flags/flag_be.gif" onclick="changeLanguage(\'fr\', true)" title="Belgique" alt="Belgique">-->\
-        <!--<img src="/images/Flags/flag_ch.gif" onclick="changeLanguage(\'de\', true)" title="Schweiz" alt="Schweiz">-->\
-        <!--<img src="/images/Flags/flag_pl.gif" onclick="changeLanguage(\'pl\', true)" title="Polska" alt="Polska">-->\
-        <!--<img src="/images/Flags/flag_cz.gif" onclick="changeLanguage(\'cs\', true)" title="Česká republika" alt="Česká republika">-->\
+        <img src="/images/Flags/flag_it.gif" onclick="changeLanguage(\'it\', true)" title="Italia" alt="Italia">\
+        <img src="/images/Flags/flag_fr.gif" onclick="changeLanguage(\'fr\', true)" title="France" alt="France">\
+        <img src="/images/Flags/flag_de.gif" onclick="changeLanguage(\'de\', true)" title="Deutschland" alt="Deutschland">\
+        <img src="/images/Flags/flag_dk.gif" onclick="changeLanguage(\'da\', true)" title="Danmark" alt="Danmark">\
+        <img src="/images/Flags/flag_se.gif" onclick="changeLanguage(\'sv\', true)" title="Sverige" alt="Sverige">\
+        <img src="/images/Flags/flag_no.gif" onclick="changeLanguage(\'no\', true)" title="Norge" alt="Norge">\
+        <img src="/images/Flags/flag_fi.gif" onclick="changeLanguage(\'fi\', true)" title="Suomi" alt="Suomi">\
+        <img src="/images/Flags/flag_ru.gif" onclick="changeLanguage(\'ru\', true)" title="Россия" alt="Россия">\
+        <img src="/images/Flags/flag_cl.gif" onclick="changeLanguage(\'es\', true)" title="Chile" alt="Chile">\
+        <img src="/images/Flags/flag_mx.gif" onclick="changeLanguage(\'es\', true)" title="México" alt="México">\
+        <img src="/images/Flags/flag_es.gif" onclick="changeLanguage(\'es\', true)" title="España" alt="España">\
+        <img src="/images/Flags/flag_hr.gif" onclick="changeLanguage(\'hr\', true)" title="Hrvatska" alt="Hrvatska">\
+        <img src="/images/Flags/flag_si.gif" onclick="changeLanguage(\'sl\', true)" title="Slovenija" alt="Slovenija">\
+        <img src="/images/Flags/flag_uk.gif" onclick="changeLanguage(\'en\', true)" title="United Kingdom" alt="United Kingdom">\
+        <img src="/images/Flags/flag_ca.gif" onclick="changeLanguage(\'en\', true)" title="Canada" alt="Canada">\
+        <img src="/images/Flags/flag_au.gif" onclick="changeLanguage(\'en\', true)" title="Australia" alt="Australia">\
+        <img src="/images/Flags/flag_za.gif" onclick="changeLanguage(\'af\', true)" title="South Africa" alt="South Africa">\
+        <img src="/images/Flags/flag_nl.gif" onclick="changeLanguage(\'nl\', true)" title="Nederland" alt="Nederland">\
+        <img src="/images/Flags/flag_be.gif" onclick="changeLanguage(\'fr\', true)" title="Belgique" alt="Belgique">\
+        <img src="/images/Flags/flag_ch.gif" onclick="changeLanguage(\'de\', true)" title="Schweiz" alt="Schweiz">\
+        <img src="/images/Flags/flag_pl.gif" onclick="changeLanguage(\'pl\', true)" title="Polska" alt="Polska">\
+        <img src="/images/Flags/flag_cz.gif" onclick="changeLanguage(\'cs\', true)" title="Česká republika" alt="Česká republika">\
         <img src="/images/Flags/flag_ja.gif" onclick="changeLanguage(\'ja\', true)" title="日本" alt="日本">\
     </div>\
 \
