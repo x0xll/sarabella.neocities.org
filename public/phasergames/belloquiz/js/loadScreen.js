a4d60eed3cfbc2be39fe37d391c60c2827dca50e
Galahane Rouillé-Poirel <galahane.rouille@gmail.com>
2025-02-10 14:15:47 +0100

    Load questions datas from correct json

--- public/phasergames/belloquiz/js/loadScreen.js
+++ public/phasergames/belloquiz/js/loadScreen.js
@@ -15,24 +15,10 @@ class Load extends Phaser.Scene
         const game = this;
 
         // Loading the questions
-        game.questionDatas = []
-        // Get the possible questions
-        const xmlHttp = new XMLHttpRequest();
-        xmlHttp.onload = function() {
-            game.questionDatas = JSON.parse(this.responseText).questions;
-
-            game.questionDatas.forEach(question => {     
-                game.load.image(`${game.questionDatas.indexOf(question)}_img`, `${question.image}`);
-            });
-        }
-
-        let datasLang = lang;
-
-        if (!urlExists(`./datas/${datasLang}.json`))
-            datasLang = "en"
-
-        xmlHttp.open("GET", `./datas/${datasLang}.json`);
-        xmlHttp.send();
+        game.questionDatas = langData.questions;
+        game.questionDatas.forEach(question => {     
+            game.load.image(`${game.questionDatas.indexOf(question)}_img`, `${question.image}`);
+        });
     }
 
     create ()
