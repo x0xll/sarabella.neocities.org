c8c12692ccc11d660b1656658a4ea854abf08744
SinGala <galahane.rouille@gmail.com>
2025-04-09 16:56:59 +0200

    Update navbar

--- public/header.js
+++ public/header.js
@@ -1,20 +1,19 @@
-document.write('\
-\
-    <div class="header">\
-    <a href="/play.html">\
-        <button class="navbutt" data-il8n="header_play_btn">Play!</button></a>\
-    <a href="/community.html">\
-        <button class="navbutt" data-il8n="header_ressources_btn">Resources</button></a>\
-    <a href="/devlog.html">\
-        <button class="navbutt" data-il8n="header_updates_btn">Updates</button></a>\
-    <a id="logo" href="/index.html">\
-        <img src="/images/besalogo.png" style="padding: 20px, 0px; vertical-align: middle"></a>\
-    <a href="https://bellasara.com/">\
-        <button class="navbutt" data-il8n="header_besaofficial_btn">BeSa Official</button></a> \
-    <a href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki">\
-        <button class="navbutt" data-il8n="header_wiki_btn">Wiki.gg</button></a>\
-    <a href="https://discord.com/invite/8SJzaBcAhD">\
-        <button class="navbutt" data-il8n="header_discord_btn">Discord</button></a>\
-    </div>\
-\
-');
+document.write('<div class="header">\
+<ul id="nav">\
+<li><a href="/play.html"><img src="/images/nav/Icons/adventure_logo.png" title="Play!" class="navMainIcon"></a></li>\
+<li><a href="/community.html"><img src="/images/nav/Icons/wardrobe_logo.png" title="Resources" class="navMainIcon"></a></li>\
+<li><a href="/devlog.html"><img src="/images/nav/Icons/stable_logo.png" title="Updates" class="navMainIcon"></a></li>\
+<a id="logo" href="/index.html">\
+<img src="/images/besalogo.png" style="padding: 20px, 0px; vertical-align: middle"></a>\
+<li><a href="https://bellasara.com/" target="_blank"><img src="/images/nav/Icons/besa_icon.png" title="BeSa Official" class="navMainIcon"></a></li>\
+<li><a href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki" target="_blank"><img src="/images/nav/Icons/wiki_logo.png" title="Wiki.gg" class="navMainIcon"></a></li>\
+<li><a href="https://discord.com/invite/8SJzaBcAhD" target="_blank"><img src="/images/nav/Icons/discord_icon.png" title="Discord" class="navMainIcon"></a></li>\
+</ul>\
+</div>\
+<div id="login" style="height: fit-content; width: fit-content;">\
+<select name="currentUserDropdown" id="currentUserDropdown" onchange="chooseUser()">\
+<option value="guest">Guest</option>\
+<option value="create">Create User</option>\
+</select>\
+<p id="horseshoes">10000 <img src="/images/nav/Horseshoe.png"></p>\
+</div>');
