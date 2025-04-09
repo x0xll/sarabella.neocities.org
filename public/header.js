83808a5037c7da71d87fde60e432b2598109dc2b
Gridloch <margaretwalker999@gmail.com>
2025-04-09 22:22:10 +0100

    Start updating header

--- public/header.js
+++ public/header.js
@@ -1,19 +1,47 @@
-document.write('<div class="header">\
-<ul id="nav">\
-<li><a href="/play.html"><img src="/images/nav/Icons/adventure_logo.png" title="Play!" class="navMainIcon"></a></li>\
-<li><a href="/community.html"><img src="/images/nav/Icons/wardrobe_logo.png" title="Resources" class="navMainIcon"></a></li>\
-<li><a href="/devlog.html"><img src="/images/nav/Icons/stable_logo.png" title="Updates" class="navMainIcon"></a></li>\
-<a id="logo" href="/index.html">\
-<img src="/images/besalogo.png" style="padding: 20px, 0px; vertical-align: middle"></a>\
-<li><a href="https://bellasara.com/" target="_blank"><img src="/images/nav/Icons/besa_icon.png" title="BeSa Official" class="navMainIcon"></a></li>\
-<li><a href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki" target="_blank"><img src="/images/nav/Icons/wiki_logo.png" title="Wiki.gg" class="navMainIcon"></a></li>\
-<li><a href="https://discord.com/invite/8SJzaBcAhD" target="_blank"><img src="/images/nav/Icons/discord_icon.png" title="Discord" class="navMainIcon"></a></li>\
-</ul>\
+document.write('<div class="header-container">\
+<div class="header">\
+<a id="logo" href="/index.html"><img src="/images/besalogo.png" id="logo-img"></a>\
+\
+<div class="nav">\
+<div class="dropMenu">\
+<img src="/images/nav/Icons/castle_logo.png" title="Play!">\
+<div>Explore</div>\
 </div>\
+<div class="navContent">\
+<div class="navIcon">\
+<a class="navMainIcon" href="/play.html"><img src="/images/nav/Icons/adventure_logo.png" title="Play!" class="navMainIcon"><br></a>\
+<a class="navIconText" href="/play.html">Play</a>\
+</div>\
+<div class="navIcon">\
+<a class="navMainIcon" href="/community.html"><img src="/images/nav/Icons/resource_logo.png" title="Resources" class="navMainIcon"><br></a>\
+<a class="navIconText" href="/community.html">Resources</a>\
+</div>\
+<div class="navIcon">\
+<a class="navMainIcon" href="/devlog.html"><img src="/images/nav/Icons/stable_logo.png" title="Updates" class="navMainIcon"><br></a>\
+<a class="navIconText" href="/devlog.html">Updates</a>\
+</div>\
+<div class="navIcon">\
+<a class="navMainIcon" href="https://bellasara.com/"><img src="/images/nav/Icons/besa_icon.png" title="Bella Sara Official Site!" class="navMainIcon"><br></a>\
+<a class="navIconText" href="https://bellasara.com/">BeSa Official</a>\
+</div>\
+<div class="navIcon">\
+<a class="navMainIcon" href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki"><img src="/images/nav/Icons/wiki_logo.png" title="Bella Sara Wiki" class="navMainIcon"><br></a>\
+<a class="navIconText" href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki">BeSa Wiki</a>\
+</div>\
+<div class="navIcon">\
+<a class="navMainIcon" href="https://discord.com/invite/8SJzaBcAhD"><img src="/images/nav/Icons/discord_icon.png" title="Bella Sara Discord" class="navMainIcon"><br></a>\
+<a class="navIconText" href="https://discord.com/invite/8SJzaBcAhD">BeSa Discord</a>\
+</div>\
+</div>\
+</div>\
+\
 <div id="login" style="height: fit-content; width: fit-content;">\
 <select name="currentUserDropdown" id="currentUserDropdown" onchange="chooseUser()">\
 <option value="guest">Guest</option>\
 <option value="create">Create User</option>\
 </select>\
 <p id="horseshoes">10000 <img src="/images/nav/Horseshoe.png"></p>\
+</div>\
+\
+</div>\
 </div>');
