284c43d2f05093716872bbb44df890ff064e21d9
SinGala <galahane.rouille@gmail.com>
2025-04-11 17:53:03 +0200

    Revert "Link Jumping Game Phaser with fake user horseshoes/level reached"
    This reverts commit 98e8e49873240b9081e39da40fb6d05e36ab6243.

--- public/phasergames/stablesrecreation/js/loadScreen.js
+++ public/phasergames/stablesrecreation/js/loadScreen.js
@@ -1,7 +1,6 @@
-nextScreen = false
+// This scene is just used to load the image for the loading screen                
 
-
-
+// Load screen scene
 class Load extends Phaser.Scene 
 {
     constructor ()
@@ -11,50 +10,42 @@ class Load extends Phaser.Scene
 
     preload ()
     {
-        this.load.audio('background_music', ['./sounds/game_soundtrack.mp3']);
-        this.load.audio('horseshoe_sound', ['./sounds/497.mp3']);
-        this.load.audio('canter_sound', ['./sounds/1550_canter.mp3']);
-        this.load.audio('buttslide_sound', ['./sounds/1553_buttslide.mp3']);
-        this.load.audio('running_sound', ['./sounds/1556_run.mp3']);
-        this.load.audio('blue_gem_sound', ['./sounds/1559_blueGemAudio.mp3']);
-        this.load.audio('pink_gem_sound', ['./sounds/1562_pinkGemAudio.mp3']);
-        this.load.audio('yellow_gem_sound', ['./sounds/1565_yellowGemAudio.mp3']);
-
+        this.load.image('card_back', './images/selector/card_back.png');
     }
 
     create ()
-    {
-        this.playMusic = true
-        this.levelUnlocked = [true, false, false, false]
-        let savedLoadedLevel = loadDatas("level", "SpectacularJumpingGame");
-        for (let i = 0; i <= savedLoadedLevel; i++)
-        {
-            // TODO : Remove this when the levels are finished
-            if (i > 1) continue;
+    { 
+        if (loadInto === "Stables") {
 
-            this.levelUnlocked[i] = true;
+            let langFile = null
+            if (horseData.type === 'dressup') {
+                langFile = `./lang/dressup`
+            } else {
+                langFile = `./lang/${horseData.type}`
+            }
+            if (!locale || !urlExists(`${langFile}_${locale}.json`)) {
+                locale ='en'
+            }
+            const xmlHttplocale = new XMLHttpRequest();
+            xmlHttplocale.onload = function() {
+                const myObj = JSON.parse(this.responseText);
+                localeData = myObj
+            }
+            xmlHttplocale.open("GET", `${langFile}_${locale}.json`);
+            xmlHttplocale.send();
+
+            if (horseData.type !== 'dressup'){
+            const xmlHttpEnglish = new XMLHttpRequest();
+            xmlHttpEnglish.onload = function() {
+                const myObj = JSON.parse(this.responseText);
+                englishDatas = myObj
+            }
+            xmlHttpEnglish.open("GET", `${langFile}_en.json`);
+            xmlHttpEnglish.send();
+            }
+
+            loadInto = `${horseData.type}Stable`
         }
-
-        if (this.physics.config.debug) { 
-            this.levelUnlocked = [true, true, true, false]
-        }
-
-        // Music
-        this.backgroundMusic = this.sound.add('background_music');
-        this.backgroundMusic.loop = true; 
-        this.backgroundMusic.play();
-
-        this.canterSound = this.sound.add('canter_sound');
-        this.canterSound.loop = true;
-        this.buttslideSound = this.sound.add('buttslide_sound');
-        this.buttslideSound.loop = true;
-        this.runningSound = this.sound.add('running_sound');
-        this.runningSound.loop = true;
-        this.horseshoeSound = this.sound.add('horseshoe_sound');
-        this.blueGemSound = this.sound.add('blue_gem_sound');
-        this.pinkGemSound = this.sound.add('pink_gem_sound');
-        this.yellowGemSound = this.sound.add('yellow_gem_sound');
-
-        this.scene.start('StartScreen', {backgroundMusic: this.backgroundMusic, canterSound: this.canterSound, buttslideSound: this.buttslideSound, runningSound: this.runningSound, horseshoeSound: this.horseshoeSound, blueGemSound: this.blueGemSound, pinkGemSound: this.pinkGemSound, yellowGemSound: this.yellowGemSound, playMusic: this.playMusic, levelUnlocked: this.levelUnlocked, started: false});
+        this.scene.start(loadInto);
     }
 }
