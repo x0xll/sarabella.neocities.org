09ebed18d6737b86e21b378a7b266113a3fde85f
SinGala <galahane.rouille@gmail.com>
2025-04-15 17:07:19 +0200

    Update text signup + link to loca

--- public/header.js
+++ public/header.js
@@ -5,32 +5,32 @@ document.write('<div class="header-container">\
 <div class="nav">\
 <div class="dropMenu">\
 <img src="/images/nav/Icons/castle_logo.png" title="Explore">\
-<div>Explore</div>\
+<div data-il8n="header_explore_btn">Explore</div>\
 </div>\
 <div class="navContent">\
 <div class="navIcon">\
 <a class="navMainIcon" href="/play.html"><img src="/images/nav/Icons/adventure_logo.png" title="Play!" class="navMainIcon"><br></a>\
-<a class="navIconText" href="/play.html">Play</a>\
+<a class="navIconText" href="/play.html" data-il8n="header_play_btn">Play</a>\
 </div>\
 <div class="navIcon">\
 <a class="navMainIcon" href="/community.html"><img src="/images/nav/Icons/resource_logo.png" title="Resources" class="navMainIcon"><br></a>\
-<a class="navIconText" href="/community.html">Resources</a>\
+<a class="navIconText" href="/community.html" data-il8n="header_resources_btn">Resources</a>\
 </div>\
 <div class="navIcon">\
 <a class="navMainIcon" href="/devlog.html"><img src="/images/nav/Icons/stable_logo.png" title="Updates" class="navMainIcon"><br></a>\
-<a class="navIconText" href="/devlog.html">Updates</a>\
+<a class="navIconText" href="/devlog.html" data-il8n="header_updates_btn">Updates</a>\
 </div>\
 <div class="navIcon">\
 <a class="navMainIcon" href="https://bellasara.com/"><img src="/images/nav/Icons/besa_icon.png" title="Bella Sara Official Site!" class="navMainIcon"><br></a>\
-<a class="navIconText" href="https://bellasara.com/">BeSa Official</a>\
+<a class="navIconText" href="https://bellasara.com/" data-il8n="header_besaofficial_btn">BeSa Official</a>\
 </div>\
 <div class="navIcon">\
 <a class="navMainIcon" href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki"><img src="/images/nav/Icons/wiki_logo.png" title="Bella Sara Wiki" class="navMainIcon"><br></a>\
-<a class="navIconText" href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki">BeSa Wiki</a>\
+<a class="navIconText" href="https://bellasara.wiki.gg/wiki/Bella_Sara_Wiki"  data-il8n="header_wiki_btn">BeSa Wiki</a>\
 </div>\
 <div class="navIcon">\
 <a class="navMainIcon" href="https://discord.com/invite/8SJzaBcAhD"><img src="/images/nav/Icons/discord_icon.png" title="Bella Sara Discord" class="navMainIcon"><br></a>\
-<a class="navIconText" href="https://discord.com/invite/8SJzaBcAhD">BeSa Discord</a>\
+<a class="navIconText" href="https://discord.com/invite/8SJzaBcAhD" data-il8n="header_discord_btn">BeSa Discord</a>\
 </div>\
 </div>\
 </div>\
@@ -38,8 +38,8 @@ document.write('<div class="header-container">\
 <div id="login" style="height: fit-content; width: fit-content;">\
 <img src="/images/nav/NamePlaque.png" style="width: 100%" title="username" class="NamePlaque">\
 <select name="currentUserDropdown" id="currentUserDropdown" onchange="chooseUser(true)">\
-<option value="guest">Guest</option>\
-<option value="create">Create User</option>\
+<option value="guest" data-il8n="user_guest">Guest</option>\
+<option value="create" data-il8n="user_create">Create User</option>\
 </select>\
 <p id="horseshoes">10000 <img src="/images/nav/Horseshoe.png"></p>\
 </div>\
