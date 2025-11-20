cea9262cae2522de4cc2354b7268bcb124fcfeae
SinGala <104780578+Sin-Gala@users.noreply.github.com>
2025-11-20 01:27:19 +0100

    Clean up canterfarm script

--- public/phasergames/adventures/scripts/world/canterfarm.js
+++ public/phasergames/adventures/scripts/world/canterfarm.js
@@ -19,11 +19,6 @@ class World_CanterFarm extends Phaser.Scene
         // Zone vars
         game.zoneImgLoaded = false;
 
-        // //TEST LOADING
-        // this.load.image("Test", `${LEVEL_ASSETS_PATH}${game.AREA_NAME}/T0053c/1.png`)
-
-        // TODO : Load the background
-
         // Parse the zone file
         // TODO : See if possible to have this function be in an external file to the scene
         // This would be used on all world scenes, while only changing the zone file url
@@ -92,9 +87,6 @@ class World_CanterFarm extends Phaser.Scene
             // TODO : Instantiate entities (player, npcs, plants)
             game.playerObj.instantiatePlayerSprites();
 
-            // TEST DIALOGUE
-            //showDialogue(game, {name: "Cade Traveler", id: "C001"}, "Happy day! The bridge is safe and sound thanks to you. Here are some horseshoes for your help. Oh my! It looks like that wolf over there has captured the Starstone otter. Go see if you can help her out.", undefined);
-
             // TEST INVENTORY
             //showInventory(game);
         }
@@ -135,21 +127,6 @@ class World_CanterFarm extends Phaser.Scene
         instantiateWorld();
         game.playerObj.move();
         game.timeManager.renderDayNight()
-
-        // TODO: Clean up, this is a temporary quest test
-        let spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        spaceBar.on('down', () => 
-        {
-            /*var status = getQuestStatus(game, "ADS-0000000825", "ADV-0000000899", "QUE-0000002105");
-
-            if (status === QUEST_STATES.WAITING)
-                startQuest(game, "ADS-0000000825", "ADV-0000000899", "QUE-0000002105")
-            else if (status === QUEST_STATES.STARTED)
-            {
-                finishQuest(game, "ADS-0000000825", "ADV-0000000899", "QUE-0000002105");
-                startQuest(game, "ADS-0000000825", "ADV-0000000902", "QUE-0000002117");
-            }*/
-        })
     }
 
     update() 
@@ -159,6 +136,6 @@ class World_CanterFarm extends Phaser.Scene
         game.playerObj.updatePlayer();
         game.timeManager.updateTime();
 
-        debug_DrawTriggerQuest(game);
+        //debug_DrawTriggerQuest(game);
     }
 }
