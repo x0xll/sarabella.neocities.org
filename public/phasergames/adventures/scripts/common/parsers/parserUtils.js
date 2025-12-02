38c98e0059bba956d2b89285e0df8788a5acb5f6
Galahane Rouillé-Poirel <galahane.rouille@gmail.com>
2025-12-02 14:26:17 -0300

    Update parseZoneXML to use the general node parser

--- public/phasergames/adventures/scripts/common/parsers/parserUtils.js
+++ public/phasergames/adventures/scripts/common/parsers/parserUtils.js
@@ -1,81 +1,12 @@
 function parseZoneXML(xmlObj)
 {
-    //console.log(xmlObj);
+    const result = {}
 
-    var result = [];
+    xmlObj.querySelectorAll("map").forEach(map => {
+        parseXMLNode(map, result)
+    })
 
-    // Get the level grid
-    var zoneDatas = [];
-    xmlObj.querySelectorAll("levelRow").forEach(row => {
-        var rowCells = row.textContent.split(",");
-        zoneDatas.push(rowCells);
-    });
-    result.push(zoneDatas);
-
-    // Get the datas for each tile
-    var tileDatas = {};
-    xmlObj.querySelectorAll("tile").forEach(tile =>{
-        if (tileDatas[tile.attributes[0].value] != undefined)
-            console.error("Tried setting multiple time the same tile datas: " + tile.attributes[0].value);
-        else
-        {
-            var data = {};
-
-            // Tile attributes
-            for (var i = 0; i < tile.attributes.length; i++)
-            {
-                switch(tile.attributes[i].name)
-                {
-                    default:
-                        break;
-                    case "id":
-                        data.id = tile.attributes[i].value;
-                        break;
-                    case "file":
-                        data.file = tile.attributes[i].value;
-                        break;
-                    case "walkable":
-                        data.walkable = tile.attributes[i].value;
-                        break;
-                    // TODO : What can those represent, and how can we use them?
-                    case "entities":
-                        data.entity = tile.attributes[i].value;
-                        break;
-                    // TODO : What elements in the game use this value? Do we actually need it?
-                    case "isBillboard":
-                        data.billboard = tile.attributes[i].billboard;
-                        break;
-                }
-            }
-
-            // Grid kid datas
-            if (tile.children[0]) {
-
-                var grid = tile.children[0];
-                for (var i = 0; i < grid.attributes.length; i++)
-                    {
-                        switch(grid.attributes[i].name)
-                        {
-                            default:
-                                break;
-                            case "width":
-                                data.width = grid.attributes[i].value;
-                                break;
-                            case "height":
-                                data.height = grid.attributes[i].value;
-                                break;
-                            case "depth":
-                                data.depth = grid.attributes[i].value;
-                                break;
-                        }
-                    }
-            }
-
-            tileDatas[data.id] = data;
-        }   
-    });
-    result.push(tileDatas);
-    //console.log(tileDatas);
+    console.log(result);
 
     return result;
 }
